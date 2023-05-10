ce10b2cb377631080cb2f4068baf67a28ab94042
Fernando Calmet <fercalmet@gmail.com>
2023-05-09 21:23:09 -0500

    Add TryCatch and handle logging on host server

--- src/MyCompany.MyProduct.WebApi/Program.cs
+++ src/MyCompany.MyProduct.WebApi/Program.cs
@@ -1,9 +1,23 @@
 using MyCompany.MyProduct.WebApi;
+using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
-builder.ConfigureServices();
+Log.Information("Server Booting Up...");
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
+    builder.ConfigureServices();
 
-var app = builder.Build();
-app.ConfigureMiddleware();
+    var app = builder.Build();
+    app.ConfigureMiddleware();
 
-app.Run();
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Unhandled exception");
+}
+finally
+{
+    Log.Information("Server Shutting down...");
+    Log.CloseAndFlush();
+}
